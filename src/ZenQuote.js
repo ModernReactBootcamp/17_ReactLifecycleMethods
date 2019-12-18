bec18ab022eed97ea6e3739a2f2363e913f57714
kdord <katya.koliada49@gmail.com>
2019-12-18 10:57:02 +0200

    add styling and loading-animation to  Zenquote

--- src/ZenQuote.js
+++ src/ZenQuote.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./ZenQuote.css";
 
 class ZenQuote extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      quote: ""
+      quote: "",
+      isLoaded: false
     };
   }
 
   componentDidMount() {
     //load data
     axios.get("https://api.github.com/zen").then(response => {
-      this.setState({ quote: response.data });
+      setTimeout(
+        function() {
+          this.setState({ quote: response.data, isLoaded: true });
+        }.bind(this),
+        3000
+      );
     });
     // console.log(response.data));
     //set state with that data
@@ -21,9 +28,21 @@ class ZenQuote extends Component {
   render() {
     return (
       <div>
-        <h1>Always remember</h1>
-        <p>{this.state.quote}</p>
+        {this.state.isLoaded ? (
+          <div>
+            <h1>Always remember</h1>
+            <p>{this.state.quote}</p>
+          </div>
+        ) : (
+          <div className="loader">
+            <div className="loader-name">Loading</div>
+            <div className="loader-circle"></div>
+          </div>
+        )}
       </div>
+
+      //
+      //
     );
   }
 }
